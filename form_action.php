2d91c5a7a0b7c11f188950c42282ec5bd4d0784a
dennisisk <97694680+dennisisk@users.noreply.github.com>
2023-10-07 23:02:02 -0400

    Create form_action.php

---
+++ form_action.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>Search tool contact</title>
+</head>
+<body>
+<h1>Contact List</h1>
+First Name: <?php echo $_GET["fname"]; ?> <br>
+Last Name: <?php echo $_GET["lname"]; ?> <br>
+Gender: <?php echo $_GET["gender"] ?> <br>
+E-mail: <?php echo $_GET["email"] ?> <br>
+Address: <?php echo $_GET["address"] ?> <br>
+Comment: <?php echo $_GET["comment"] ?> <br>
+
+</body>
+</html>
